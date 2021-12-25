26eb0a91156bf62424d61a2c59aa804581f4ce36
Darío <darii0_-@hotmail.com>
2021-12-25 11:26:35 +0100

    Stage 2

--- src/main/fizzbuzz.ts
+++ src/main/fizzbuzz.ts
@@ -1,9 +1,9 @@
 export const checkFizzBuzz = (n: number) => {
 	if(n % 3 === 0 && n % 5 === 0)
 		return "FizzBuzz";
-	else if(n % 3 === 0) 
+	else if(n % 3 === 0 || n.toString().includes("3")) 
 		return "Fizz";
-	else if(n % 5 === 0)
+	else if(n % 5 === 0 || n.toString().includes("5"))
 		return "Buzz";
 	else
 		return n;
